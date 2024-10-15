9659bd9274e1d4c542a638e7f3b82ae4a396a869
sam geyer <geyerwork@gmail.com>
2024-10-15 19:17:39 +1100

    added beginning of growCursor & shrinkCursor functions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const cursor
+const cursor = document.querySelector("div.cursor")
+
+// when I hold mouse down, make cursor bigger
+const growCursor = function() {
+    cursor.classList.add("on-click")
+}
+
+// when I let go, make cursor smaller 
+const shrinkCursor = function() {
+    cursor.classList.remove("on-click")
+}
+
+growCursor()
+shrinkCursor()
